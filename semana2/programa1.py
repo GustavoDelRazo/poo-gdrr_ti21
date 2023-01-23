76e3db2f6817f9269d7ac5c8401913dff4430a13
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-01-23 16:59:13 +0000

    CREATE carpeta semana2 y programa1.py

---
+++ semana2/programa1.py
@@ -0,0 +1,9 @@
+"""
+   Programa1
+   Nombre: Gustavo DRR
+   Fecha: 23/01/2023
+   Descripcion: Comentarios multilinea, comentarios de una linea y concatenacion de cadenas
+"""
+variable1 = "Hola"  # Variable que almacena una cadena de caracteres
+variable2 = "Python"  # Variable que almacena una cadena de caracteres
+print(variable1 + variable2)# Concatena 2 cadenas
