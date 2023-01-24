f341e90b915157aa2b91df0b3ea5c6bd4b3105b5
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-01-24 19:59:28 +0000

    UPDATE se actualizo el programa1.py

---
+++ semana2/programa2.py
@@ -0,0 +1,10 @@
+"""
+   Programa2
+   Nombre: Gustavo DRR
+   Fecha: 24/01/2023
+   Descripcion: Comentarios multilinea, comentarios de una linea y concatenacion de cadenas
+"""
+variable1 = "Hola"  # Variable tipo string
+variable2 = 619  # Variable tipo int
+print("{} {}" .format(variable1, variable2)) # Permite imprimir valores sin importar el tipo de dato
+
