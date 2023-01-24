3143c2927ae6bf06ef3577df88d0edf90fdd1c23
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-01-24 20:25:50 +0000

    CREATE  programa3.py

---
+++ semana2/programa3.py
@@ -0,0 +1,16 @@
+"""
+   Programa3
+   Nombre: Gustavo DRR
+   Fecha: 24/01/2023
+   Descripcion: Comentarios multilinea, comentarios de una linea, variables int, string y 3 print       tipo format
+"""
+
+
+numero1 = 10
+numero2 = 5
+
+
+print("{} + {} = {}".format(numero1, numero2, numero1 + numero2))
+print("{} = {} * {}".format(numero1 * numero2, numero1, numero2))
+print("El resultado de dividir {} / {} = {}".format(numero1, numero2, numero1 / numero2))
+
