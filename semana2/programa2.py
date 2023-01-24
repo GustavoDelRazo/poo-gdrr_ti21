e4ddf85ea5a9174a729d2f505c85d5246dd20105
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-01-24 20:01:07 +0000

    CREATE el programa2.py y utilizar el format

--- semana2/programa2.py
+++ semana2/programa2.py
@@ -2,9 +2,10 @@
    Programa2
    Nombre: Gustavo DRR
    Fecha: 24/01/2023
-   Descripcion: Comentarios multilinea, comentarios de una linea y concatenacion de cadenas
+   Descripcion: Comentarios multilinea, comentarios de una linea, variables int, string y format
 """
 variable1 = "Hola"  # Variable tipo string
 variable2 = 619  # Variable tipo int
 print("{} {}" .format(variable1, variable2)) # Permite imprimir valores sin importar el tipo de dato
+
 
