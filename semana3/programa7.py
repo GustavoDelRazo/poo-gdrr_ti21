3276dc210780b5d33a7ae7ec377d0f292a2b1d4b
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-01-31 20:48:26 +0000

    UPDATE programa7.py

--- semana3/programa7.py
+++ semana3/programa7.py
@@ -5,11 +5,11 @@
    Descripcion: Comentarios multilinea, variables con input y prints
 """
 
-PI = 3.1416
+pi = 3.1416 #Asigna el valor de la variable PI
 radio = float(input("Ingresa el radio del circulo: ")) #Asigna el valor de la variable radio
 
-perimetro = (radio) * (2) * (PI) # Hace la operacion de la variable radio por pi y 2
-area = (PI) * (radio) ** (2) #Hace la operacion de la variable radio por pi
+perimetro = (radio) * (2) * (pi) # Hace la operacion de la variable radio por pi y 2
+area = (pi) * (radio) ** (2) #Hace la operacion de la variable radio por pi
 
 print("El area de un criculo de {} de radio es {}".format(radio, area)) # Imprime el resultado de la variable area
 print("El perimetro de un circulo de {} de radio es {}".format(radio, perimetro)) # Imprime el resultado de la variable perimetro
@@ -17,8 +17,8 @@ print("--------------------------------------------------------------")
 
 lado = float(input("Ingresa el lado del cuadrado: ")) # Asigna el valor de la variable lado
 
-perimetro = (lado) * (4) # Hace la operacion de la variable perimetro
+perimetroc = (lado) * (4) # Hace la operacion de la variable perimetro
 areac = (lado) ** (2) # Hace la operacion de la variable area
 
 print("El area de un cuadrado de {} de lado es {}".format(lado, areac)) # Imprime el resultado de la variable area
-print("El perimetro de un cuadrado de {} de lado es {}".format(lado, perimetro)) # Imprime el resultado de la variable perimetro
+print("El perimetro de un cuadrado de {} de lado es {}".format(lado, perimetroc)) # Imprime el resultado de la variable perimetro
