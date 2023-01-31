6025012c0f93d5f0282c8108b69e88ff36e90e89
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-01-31 20:32:22 +0000

    UPDATE programa7.py

--- semana3/programa7.py
+++ semana3/programa7.py
@@ -12,13 +12,13 @@ perimetro = (radio) * (2) * (3.1416) # Hace la operacion de la variable radio po
 area = (3.1416) * (radio) ** (2) #Hace la operacion de la variable radio por pi
 
 print("El area de un criculo de {} de radio es {}".format(radio, area)) # Imprime el resultado de la variable area
-print("El perimetro de un circulo de {} de radio es {}".format(radio, perimetro)) #
+print("El perimetro de un circulo de {} de radio es {}".format(radio, perimetro)) # Imprime el resultado de la variable perimetro
 print("---------------------------------------------------------------")
 
-lado = float(input("Ingresa el lado del cuadrado: ")) #
+lado = float(input("Ingresa el lado del cuadrado: ")) # Asigna el valor de la variable lado
 
-perimetro = (lado) * (4) #
-areac = (lado) ** (2) #
+perimetro = (lado) * (4) # Hace la operacion de la variable perimetro
+areac = (lado) ** (2) # Hace la operacion de la variable area
 
-print("El area de un cuadrado de {} de lado es {}".format(lado, areac)) #
-print("El perimetro de un cuadrado de {} de lado es {}".format(lado, perimetro)) #
+print("El area de un cuadrado de {} de lado es {}".format(lado, areac)) # Imprime el resultado de la variable area
+print("El perimetro de un cuadrado de {} de lado es {}".format(lado, perimetro)) # Imprime el resultado de la variable perimetro
