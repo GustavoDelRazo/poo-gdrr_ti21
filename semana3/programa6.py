9850b6ad343c5fbeb646162fab023678843379d8
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-01-31 20:29:09 +0000

    UPDATE programa6.py

--- semana3/programa6.py
+++ semana3/programa6.py
@@ -6,13 +6,13 @@
 """
 
 
-lado1 = input("Ingresa el lado 1: ") #Asigna el valor de la variable lado1
-lado2 = input("Ingresa el lado 2: ") # Asigna el valor de la variable lado2
-base = input("Ingresa la base: ") # Asigna el valor de la variable base
-altura = input("Ingresa la altura: ") # Asigane el valor de la variable altura
+lado1 = float(input("Ingresa el lado 1: ")) #Asigna el valor de la variable lado1
+lado2 = float(input("Ingresa el lado 2: ")) # Asigna el valor de la variable lado2
+base = float(input("Ingresa la base: ")) # Asigna el valor de la variable base
+altura = float(input("Ingresa la altura: ")) # Asigane el valor de la variable altura
 
-perimetro = float(lado1) + float(lado2) + float(base) #Hace la operacion de de las variables de lados y base
-area = float(base) * float(altura) / (2) #Hace la operacion de la variable base y altura
+perimetro = (lado1) + (lado2) + (base) #Hace la operacion de de las variables de lados y base
+area = (base) * (altura) / (2) #Hace la operacion de la variable base y altura
 
 print("El perimetro del triangulo es: ", perimetro) #Imprime el resultado de la variable perimetro
 print("El area del triangulo es: ", area) # Imprime el resultado de la variable area
