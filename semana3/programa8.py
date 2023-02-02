47a1e7d2259de4a9d12f25f060c4006f7b7659c4
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-02-02 20:50:23 +0000

    CREATE programa9.py

---
+++ semana3/programa8.py
@@ -0,0 +1,34 @@
+"""
+   Programa8
+   Nombre: Gustavo DRR
+   Fecha: 02/02/2023
+   Descripcion: Comentarios multilinea y if, if else y if elif else
+"""
+
+{
+  if condicion:
+      codigo
+}
+
+{
+  if condicion:
+      codigo1
+  else:
+      codigo2
+}
+
+{
+  if condicion:
+    codigo1
+elif condicion2:
+    codigo2
+else:
+    codigo3
+}
+
+{
+  if n1 > n2
+    codigo1
+elif n2 > n1
+    codigo2
+}
---
+++ semana3/programa9.py
@@ -0,0 +1,27 @@
+"""
+   Programa9
+   Nombre: Gustavo DRR
+   Fecha: 02/02/2023
+   Descripcion: Comentarios multilinea, variables con input y prints
+"""
+
+
+n1 = int(input("Ingresa el primer valor: "))
+n2 = int(input("Ingresa el segundo valor: "))
+
+"""
+if n1 > n2:
+  print("El numero mayor es: ",n1)
+elif n2 > n1:
+  print("El numero mayor es: ",n2)
+elif n2 == n1:
+  print(None)
+"""
+
+if n1 < n2:
+  print(n2)
+elif n1 == n2:
+  print(None)
+elif n2 < n1:
+  print(n1)
+  
